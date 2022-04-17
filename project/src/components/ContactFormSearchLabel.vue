36f3e5b189aaf36b9accc59a16bf3684edd77f37
Oleg Zubkov <77145335+ZubeckY@users.noreply.github.com>
2022-04-17 19:33:19 +0300

    Delete project directory

--- project/src/components/ContactFormSearchLabel.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="mb-3">
-        <label for="exampleInputSearch1" class="form-label">Поиск</label>
-        <input type="search" class="form-control" id="exampleInputSearch1">
-    </div>
-</template>
-<script></script>
-<style></style>
